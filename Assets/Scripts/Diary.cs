496675f2d8a8f098c4bd857fbd4e80b40101ba03
NiCapz <168435052+NiCapz@users.noreply.github.com>
2025-11-15 22:41:35 +0000

    pageturnbuttons

--- Assets/Scripts/Diary.cs
+++ Assets/Scripts/Diary.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Diary : MonoBehaviour
 {
-
     [SerializeField] TextMeshProUGUI page1;
     [SerializeField] TextMeshProUGUI page2;
     [SerializeField] List<string> allEntries = new List<string>();
+
+    [SerializeField] Button nextPageButton;
+    [SerializeField] Button previousPageButton;
 
     List<string> paginatedStrings = new List<string>();
     List<string> entriesToShow = new List<string>();
@@ -21,16 +25,24 @@ public class Diary : MonoBehaviour
         DoPagination();
     }
 
+    void Awake()
+    {
+        nextPageButton.onClick.AddListener(NextPage);
+        previousPageButton.onClick.AddListener(PreviousPage);
+    }
+
     void Start()
     {
         NewEntry(0);
+        NewEntry(1);
+        NewEntry(2);
+        NewEntry(3);
     }
 
     void DoPagination()
     {
         paginatedStrings.Clear();
-        
-        // regenerate text to paginate
+
         StringBuilder sb = new StringBuilder();
         foreach (string entry in entriesToShow)
         {
@@ -38,16 +50,11 @@ public class Diary : MonoBehaviour
             sb.AppendLine();
         }
         string rest = sb.ToString();
-        Debug.Log(rest);
-        
+
         while (rest.Length > 0)
         {
             int nextPageLength = 470;
             nextPageLength = Math.Min(nextPageLength, rest.Length);
-            
-            Debug.Log($"rest length {rest.Length}");
-            Debug.Log($"nextpagelengtht {nextPageLength}");
-            
             string pageString = rest.Substring(0, nextPageLength);
             paginatedStrings.Add(pageString);
             rest = rest.Substring(pageString.Length);
@@ -55,15 +62,39 @@ public class Diary : MonoBehaviour
         UpdateVisiblePages(currentPageIndex);
     }
 
-    public void TurnPage()
+    public void NextPage()
     {
-        currentPageIndex += 2;
+        Debug.Log("nextpage");
+        if (paginatedStrings.Count > currentPageIndex + 2)
+        {
+            currentPageIndex += 2;
+            UpdateVisiblePages(currentPageIndex);
+        }
+    }
+
+    public void PreviousPage()
+    {
+        Debug.Log("previouspage");
+        currentPageIndex -= 2;
+        currentPageIndex = Math.Max(currentPageIndex, 0);
         UpdateVisiblePages(currentPageIndex);
     }
 
     void UpdateVisiblePages(int leftPageIndex)
     {
-        if (paginatedStrings.Count > leftPageIndex) page1.text = paginatedStrings[leftPageIndex];
-        if (paginatedStrings.Count > leftPageIndex + 1) page2.text = paginatedStrings[leftPageIndex + 1];
+
+        if (paginatedStrings.Count + 1 >= leftPageIndex)
+        {
+            page1.text = paginatedStrings[leftPageIndex];
+        }
+        if (paginatedStrings.Count > leftPageIndex + 1)
+        {
+            page2.text = paginatedStrings[leftPageIndex + 1];
+        }
+        else
+        {
+            page2.text = "";
+        }
     }
+
 }
