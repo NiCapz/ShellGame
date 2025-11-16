e689405ee121a330a66a61d8f924364dafd98418
NiCapz <168435052+NiCapz@users.noreply.github.com>
2025-11-16 02:09:06 +0000

    moving arrows

--- Assets/Scripts/ArrowButton.cs
+++ Assets/Scripts/ArrowButton.cs
@@ -1,3 +1,4 @@
+using DigitalRuby.Tween;
 using UnityEngine;
 
 public class ArrowButton : MonoBehaviour, IClickable
@@ -5,32 +6,88 @@ public class ArrowButton : MonoBehaviour, IClickable
     [SerializeField] SpriteRenderer outline;
     [SerializeField] CameraAnimation cameraAnimation;
 
+    private SpriteRenderer spriteRenderer;
+    private AssetProvider assetProvider;
+    private Vector3 startingPos;
+    private Vector3 endPos;
     static bool atBeach = true;
 
     void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        assetProvider = FindAnyObjectByType<AssetProvider>();
         outline.enabled = false;
+        startingPos = transform.position;
+        endPos = new Vector3(7.7f, 5.31f, startingPos.z);
 
+    }
+
+    private void SetRedSprite()
+    {
+        spriteRenderer.sprite = assetProvider.arrowSprites[0];
+        outline.sprite = assetProvider.arrowOutlineSprites[0];
+    }
+
+    private void SetBlueSprite()
+    {
+        spriteRenderer.sprite = assetProvider.arrowSprites[1];
+        outline.sprite = assetProvider.arrowOutlineSprites[1];
+    }
+
+    private void EaseUp()
+    {
+        TweenFactory.Tween(null, startingPos, endPos, .5f, TweenScaleFunctions.CubicEaseInOut, t => transform.position = t.CurrentValue, t => Debug.Log("eased up arrow"));
+        TweenFactory.Tween(
+        "rotateUp",
+        0f, 180f,
+        0.5f, TweenScaleFunctions.CubicEaseInOut,
+        t =>
+        {
+            Vector3 rot = transform.eulerAngles;
+            rot.z = t.CurrentValue;
+            transform.eulerAngles = rot;
+            SetBlueSprite();
+        }
+    );
+    }
+    private void EaseDown()
+    {
+        TweenFactory.Tween(null, endPos, startingPos, .5f, TweenScaleFunctions.CubicEaseInOut, t => transform.position = t.CurrentValue, t => Debug.Log("eased down arrow"));
+        TweenFactory.Tween(
+        "rotateDown",
+        180f, 0f,
+        0.5f, TweenScaleFunctions.CubicEaseInOut,
+        t =>
+        {
+            Vector3 rot = transform.eulerAngles;
+            rot.z = t.CurrentValue;
+            transform.eulerAngles = rot;
+            SetRedSprite();
+        }
+    );
     }
 
     public void ClickOn()
     {
         Debug.Log("clicked on arrow");
-         if (atBeach)
-            {
-                cameraAnimation.EaseUp();
-            } else
-            {
-                cameraAnimation.EaseDown();
-            }
-            atBeach = !atBeach;
+        if (atBeach)
+        {
+            cameraAnimation.EaseUp();
+            EaseUp();
+        }
+        else
+        {
+            cameraAnimation.EaseDown();
+            EaseDown();
+        }
+        atBeach = !atBeach;
     }
 
-      void OnMouseEnter()
+    void OnMouseEnter()
     {
         outline.enabled = true;
         Player.AddToClickables(gameObject);
-        
+
     }
 
     void OnMouseExit()
--- Assets/Scripts/AssetProvider.cs
+++ Assets/Scripts/AssetProvider.cs
@@ -5,4 +5,8 @@ public class AssetProvider : MonoBehaviour
 {
     public List<Sprite> shellSprites = new List<Sprite>();
     public List<Sprite> shellSpritesOutline = new List<Sprite>();
+    public List<Sprite> arrowSprites = new List<Sprite>();
+    public List<Sprite> arrowOutlineSprites = new List<Sprite>();
+
+
 }
